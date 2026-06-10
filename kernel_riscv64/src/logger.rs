6603f958d440ad72a69ce7503192092a641a8a82
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2026-06-09 22:50:10 -0700

    Multiple CPUs in riscv64!

---
+++ kernel_riscv64/src/logger.rs
@@ -0,0 +1,49 @@
+use core::fmt::Write;
+
+use log::max_level;
+use riscv::{interrupt, register::sstatus};
+use spin::{Mutex, Once};
+
+use crate::console::Console;
+
+struct Logger {
+    console: Mutex<Console>,
+}
+
+impl log::Log for Logger {
+    fn enabled(&self, metadata: &log::Metadata) -> bool {
+        metadata.level() <= max_level()
+    }
+
+    fn log(&self, record: &log::Record) {
+        let interrupts_were_enabled = sstatus::read().sie();
+        if interrupts_were_enabled {
+            interrupt::disable();
+        }
+        let mut console = self.console.lock();
+        writeln!(console, "{}", record.args()).unwrap();
+        drop(console);
+        if interrupts_were_enabled {
+            unsafe { interrupt::enable() };
+        }
+    }
+
+    fn flush(&self) {}
+}
+static LOGGER: Once<Logger> = Once::new();
+
+/// # Safety
+/// Only call this function once
+pub unsafe fn init() {
+    let logger = LOGGER.call_once(|| Logger {
+        console: Mutex::new(Console::take().unwrap()),
+    });
+    // Safety: nothing else is calling this function
+    unsafe { log::set_logger_racy(logger).unwrap() };
+    // Safety: nothing else is calling this function
+    unsafe { log::set_max_level_racy(log::LevelFilter::Trace) };
+}
+
+pub unsafe fn force_unlock() {
+    unsafe { LOGGER.get().unwrap().console.force_unlock() };
+}
--- kernel_riscv64/src/main.rs
+++ kernel_riscv64/src/main.rs
@@ -1,11 +1,14 @@
 #![no_std]
 #![no_main]
 
-use core::arch::naked_asm;
+mod console;
+mod logger;
+
+use core::{arch::naked_asm, ptr::addr_of};
 
 use riscv::{
     asm::wfi,
-    interrupt::Interrupt,
+    interrupt::{self, Interrupt},
     register::{
         scause, sepc,
         sie::{self, Sie},
@@ -16,7 +19,10 @@ use riscv::{
         time,
     },
 };
-use sbi::hsm::SuspendType;
+use sbi::{
+    PhysicalAddress,
+    hsm::{HartState, SuspendType},
+};
 
 // These variables are defined in the linker script
 unsafe extern "C" {
@@ -33,7 +39,7 @@ unsafe extern "C" {
 #[unsafe(link_section = ".text._header")]
 #[unsafe(no_mangle)]
 #[unsafe(naked)]
-extern "C" fn _start() {
+unsafe extern "C" fn _start() {
     naked_asm!(
         "
             j {start}
@@ -42,7 +48,7 @@ extern "C" fn _start() {
     )
 }
 #[unsafe(naked)]
-extern "C" fn start() {
+unsafe extern "C" fn start() {
     naked_asm!(
         "
         lla t0, _start
@@ -102,38 +108,74 @@ extern "C" fn start() {
     )
 }
 
+#[unsafe(naked)]
+unsafe extern "C" fn other_hart_start() -> ! {
+    naked_asm!(
+        "
+        lla sp, __stack_top
+        li t0, 64 * 1024      # 32 KiB, 1/4 of reserved stack space
+        mul t0, t0, a1        # t0 = stack_number * stack_size
+        sub sp, sp, t0
+        j {rust}
+        ",
+        rust = sym other_hart_main
+    )
+}
+
 extern "C" fn kernel_main(hart_id: usize, fdt_addr: usize) -> ! {
+    unsafe { logger::init() };
+    log::info!("Hello World!");
+
+    let mut hart_id = 0;
+    let mut stack_number = 1; // our stack number is 0
+    while let Ok(state) = sbi::hsm::hart_state(hart_id) {
+        log::info!("Hart {hart_id} state: {state:?}");
+        if state == HartState::Stopped {
+            unsafe {
+                sbi::hsm::hart_start(
+                    hart_id,
+                    PhysicalAddress::new(other_hart_start as *const () as usize),
+                    stack_number,
+                )
+            }
+            .unwrap();
+            stack_number += 1;
+        }
+        hart_id += 1;
+    }
+
     unsafe {
         stvec::write(Stvec::new(
             kernel_entry as *const () as usize,
             TrapMode::Direct,
         ))
     };
-    let mut sstatus = Sstatus::from_bits(0);
-    sstatus.set_sie(true);
-    unsafe { sstatus::write(sstatus) };
+    unsafe { interrupt::enable() };
 
     let time = time::read64();
     sbi::timer::set_timer(time + 10000).unwrap();
 
-    let msg = "Hello World!\r\n";
-    for &byte in msg.as_bytes() {
-        sbi::legacy::console_putchar(byte);
-    }
-
-    let mut sie = Sie::from_bits(0);
-    sie.set_stimer(true);
-    unsafe { sie::write(sie) };
+    unsafe { sie::set_stimer() };
 
     loop {
-        sbi::legacy::console_putchar(b'.');
-        wfi();
+        log::info!(".");
+        // wfi();
+        unsafe { sbi::hsm::hart_suspend(SuspendType::DefaultRetentive) }.unwrap();
     }
     // sbi::legacy::shutdown()
 }
 
+unsafe extern "C" fn other_hart_main(hart_id: usize, extra_input: usize) {
+    log::info!("Hello from hart {hart_id} with extra input {extra_input}");
+
+    sbi::hsm::hart_stop().unwrap();
+}
+
 #[panic_handler]
-fn rust_panic(_info: &core::panic::PanicInfo) -> ! {
+fn rust_panic(info: &core::panic::PanicInfo) -> ! {
+    interrupt::disable();
+    unsafe { logger::force_unlock() };
+    log::error!("{info}");
     loop {}
 }
 
@@ -178,77 +220,77 @@ extern "C" fn kernel_entry() {
         "
             .align 4
             csrw sscratch, sp
-            addi sp, sp, -4 * 31
+            addi sp, sp, -8 * 31
 
-            sd ra,  4 * 0(sp)
-            sd gp,  4 * 1(sp)
-            sd tp,  4 * 2(sp)
-            sd t0,  4 * 3(sp)
-            sd t1,  4 * 4(sp)
-            sd t2,  4 * 5(sp)
-            sd t3,  4 * 6(sp)
-            sd t4,  4 * 7(sp)
-            sd t5,  4 * 8(sp)
-            sd t6,  4 * 9(sp)
-            sd a0,  4 * 10(sp)
-            sd a1,  4 * 11(sp)
-            sd a2,  4 * 12(sp)
-            sd a3,  4 * 13(sp)
-            sd a4,  4 * 14(sp)
-            sd a5,  4 * 15(sp)
-            sd a6,  4 * 16(sp)
-            sd a7,  4 * 17(sp)
-            sd s0,  4 * 18(sp)
-            sd s1,  4 * 19(sp)
-            sd s2,  4 * 20(sp)
-            sd s3,  4 * 21(sp)
-            sd s4,  4 * 22(sp)
-            sd s5,  4 * 23(sp)
-            sd s6,  4 * 24(sp)
-            sd s7,  4 * 25(sp)
-            sd s8,  4 * 26(sp)
-            sd s9,  4 * 27(sp)
-            sd s10, 4 * 28(sp)
-            sd s11, 4 * 29(sp)
+            sd ra,  8 * 0(sp)
+            sd gp,  8 * 1(sp)
+            sd tp,  8 * 2(sp)
+            sd t0,  8 * 3(sp)
+            sd t1,  8 * 4(sp)
+            sd t2,  8 * 5(sp)
+            sd t3,  8 * 6(sp)
+            sd t4,  8 * 7(sp)
+            sd t5,  8 * 8(sp)
+            sd t6,  8 * 9(sp)
+            sd a0,  8 * 10(sp)
+            sd a1,  8 * 11(sp)
+            sd a2,  8 * 12(sp)
+            sd a3,  8 * 13(sp)
+            sd a4,  8 * 14(sp)
+            sd a5,  8 * 15(sp)
+            sd a6,  8 * 16(sp)
+            sd a7,  8 * 17(sp)
+            sd s0,  8 * 18(sp)
+            sd s1,  8 * 19(sp)
+            sd s2,  8 * 20(sp)
+            sd s3,  8 * 21(sp)
+            sd s4,  8 * 22(sp)
+            sd s5,  8 * 23(sp)
+            sd s6,  8 * 24(sp)
+            sd s7,  8 * 25(sp)
+            sd s8,  8 * 26(sp)
+            sd s9,  8 * 27(sp)
+            sd s10, 8 * 28(sp)
+            sd s11, 8 * 29(sp)
 
             csrr a0, sscratch
-            sd a0, 4 * 30(sp)
+            sd a0, 8 * 30(sp)
 
             mv a0, sp
             call {handle_trap}
 
-            ld ra,  4 * 0(sp)
-            ld gp,  4 * 1(sp)
-            ld tp,  4 * 2(sp)
-            ld t0,  4 * 3(sp)
-            ld t1,  4 * 4(sp)
-            ld t2,  4 * 5(sp)
-            ld t3,  4 * 6(sp)
-            ld t4,  4 * 7(sp)
-            ld t5,  4 * 8(sp)
-            ld t6,  4 * 9(sp)
-            ld a0,  4 * 10(sp)
-            ld a1,  4 * 11(sp)
-            ld a2,  4 * 12(sp)
-            ld a3,  4 * 13(sp)
-            ld a4,  4 * 14(sp)
-            ld a5,  4 * 15(sp)
-            ld a6,  4 * 16(sp)
-            ld a7,  4 * 17(sp)
-            ld s0,  4 * 18(sp)
-            ld s1,  4 * 19(sp)
-            ld s2,  4 * 20(sp)
-            ld s3,  4 * 21(sp)
-            ld s4,  4 * 22(sp)
-            ld s5,  4 * 23(sp)
-            ld s6,  4 * 24(sp)
-            ld s7,  4 * 25(sp)
-            ld s8,  4 * 26(sp)
-            ld s9,  4 * 27(sp)
-            ld s10, 4 * 28(sp)
-            ld s11, 4 * 29(sp)
+            ld ra,  8 * 0(sp)
+            ld gp,  8 * 1(sp)
+            ld tp,  8 * 2(sp)
+            ld t0,  8 * 3(sp)
+            ld t1,  8 * 4(sp)
+            ld t2,  8 * 5(sp)
+            ld t3,  8 * 6(sp)
+            ld t4,  8 * 7(sp)
+            ld t5,  8 * 8(sp)
+            ld t6,  8 * 9(sp)
+            ld a0,  8 * 10(sp)
+            ld a1,  8 * 11(sp)
+            ld a2,  8 * 12(sp)
+            ld a3,  8 * 13(sp)
+            ld a4,  8 * 14(sp)
+            ld a5,  8 * 15(sp)
+            ld a6,  8 * 16(sp)
+            ld a7,  8 * 17(sp)
+            ld s0,  8 * 18(sp)
+            ld s1,  8 * 19(sp)
+            ld s2,  8 * 20(sp)
+            ld s3,  8 * 21(sp)
+            ld s4,  8 * 22(sp)
+            ld s5,  8 * 23(sp)
+            ld s6,  8 * 24(sp)
+            ld s7,  8 * 25(sp)
+            ld s8,  8 * 26(sp)
+            ld s9,  8 * 27(sp)
+            ld s10, 8 * 28(sp)
+            ld s11, 8 * 29(sp)
 
-            ld sp,  4 * 30(sp)
+            ld sp,  8 * 30(sp)
             sret
         ",
         handle_trap = sym handle_trap
@@ -259,10 +301,7 @@ extern "C" fn handle_trap(_trap_frame: &TrapFrame) {
     let scause = scause::read();
     let stval = stval::read();
     let user_pc = sepc::read();
-    let msg = "Timer Interrupt!\r\n";
-    for &byte in msg.as_bytes() {
-        sbi::legacy::console_putchar(byte);
-    }
+    log::info!("Timer Interrupt!");
     unsafe { sip::clear_pending(Interrupt::SupervisorTimer) };
     let time = time::read64();
     sbi::timer::set_timer(time + 10000000).unwrap();
